9b5402990951c64fe63819a957920afd4a826716
Maksonio <32421746+iBearchic@users.noreply.github.com>
2024-05-09 02:31:14 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_ind
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 539822853 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    # Выполняем t-тест для двух независимых выборок
+    t_stat, p_value = ttest_ind(x, y, equal_var=False)
+    
+    # Уровень значимости (alpha)
+    alpha = 0.05
+    
+    # Возвращаем True, если p-значение меньше alpha (различия статистически значимы)
+    return p_value < alpha
